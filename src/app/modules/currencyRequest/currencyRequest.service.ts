5e8a10c648916de6e2a72640502dede737673458
naimur53 <naimurrhman53@gmail.com>
2024-11-01 17:14:51 +0600

    doe

--- src/app/modules/currencyRequest/currencyRequest.service.ts
+++ src/app/modules/currencyRequest/currencyRequest.service.ts
@@ -167,7 +167,7 @@ const createCurrencyRequestWithPayStack = async (
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const payStackWebHook = async (data: any): Promise<void> => {
   console.log(data, 'from flutter wave');
-  const order_id = data.data.txRef.split('_$_')[1];
+  const order_id = data.data.data.tx_ref.split('_$_')[1];
   console.log({ order_id });
   const payment_status = 'finished';
   const isCurrencyRequestExits = await prisma.currencyRequest.findUnique({
